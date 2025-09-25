1db56134826c4b27a02fec809131a7b44eeff741
paridhi <paridhiawasthi20@gmail.com>
2025-09-25 12:56:02 +0530

    nnnn

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: './', // ✅ important for Render/Static hosting
+    base: './', // ✅ important for Render
     server: {
       port: 3000,
       host: '0.0.0.0',
